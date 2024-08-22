b8b4ea75db68440c01b157761b776b60d4b33cdf
blackmagic919 <actuatorspark@gmail.com>
2024-08-22 18:02:54 -0400

    Added NextJs Config to Lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,14 @@ const { resolve } = require('node:path');
 const project = resolve(__dirname, 'tsconfig.json');
 
 module.exports = {
+  root: true,
+  extends: [
+    require.resolve('@vercel/style-guide/eslint/node'),
+    require.resolve('@vercel/style-guide/eslint/typescript'),
+    require.resolve('@vercel/style-guide/eslint/browser'),
+    require.resolve('@vercel/style-guide/eslint/react'),
+    require.resolve('@vercel/style-guide/eslint/next'),
+  ],
   rules: {} //No Rules
   /*
   root: true,
